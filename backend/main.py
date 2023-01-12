1e282360ac9bddf4c61eb91847a0da2f5359e417
03hlevitt <03hlevitt@gmail.com>
2023-01-12 15:41:28 +0000

    init values

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
 import sqlite3
 
 
-def view_orders(name):
+def view_orders(customer_id):
     conn = sqlite3.connect('orders.db')
     c = conn.cursor()
-    c.execute("SELECT * FROM orders")
+    c.execute("SELECT * FROM orders WHERE customer_id = '%s'", customer_id)
     rows = c.fetchall()
     conn.close()
     return rows
+
+
+if __name__ == '__main__':
+    print(view_orders(1))
