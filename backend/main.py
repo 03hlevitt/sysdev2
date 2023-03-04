bf7d1e7b580a9d51a61088e6c9218fee01a82072
Harrison Levitt <03hlevitt@gmail.com>
2023-03-04 11:15:52 +0000

    vs code being weird and changing to a class

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,6 @@
 import sqlite3
 
-def sql_attempt(sql):
-    conn = sqlite3.connect('orders.db')
-    c = conn.cursor()
-    c.execute(sql)
-    rows = c.fetchall()
-    conn.commit()
-    conn.close()
-    return rows
 
-def execute_sql(sql):
-    try:
-        return sql_attempt(sql)
-    except sqlite3.OperationalError as e:
-        print(e)
-        init_tables()
-        return sql_attempt(sql)
 
 def view_orders_by_customer_id(customer_id):
     conn = sqlite3.connect('orders.db')
@@ -25,34 +10,51 @@ def view_orders_by_customer_id(customer_id):
     conn.close()
     return rows
 
+class Menu:
+    def __sql_attempt(self, sql):
+        conn = sqlite3.connect('orders.db')
+        c = conn.cursor()
+        c.execute(sql)
+        rows = c.fetchall()
+        conn.commit()
+        conn.close()
+        return rows
 
-def create_menu_item(name, price):
-    sql = "INSERT INTO menu_items (name, price) VALUES ('%s', '%s')" % (name, price)
-    print("insert sql: ", sql)
-    return execute_sql(sql)
+    def __execute_sql(self, sql):
+        try:
+            return self.__sql_attempt(sql)
+        except sqlite3.OperationalError as e:
+            print(e)
+            init_tables()
+            return self.__sql_attempt(sql)
+
+    def create_menu_item(self, name, price):
+        sql = "INSERT INTO menu_items (name, price) VALUES ('%s', '%s')" % (name, price)
+        print("insert sql: ", sql)
+        return self.__execute_sql(sql)
 
 
-def view_menu_items(**kwargs):
-    sql = "SELECT * FROM menu_items"
-    if kwargs:
-        sql += " WHERE "
+    def view_menu_items(self, **kwargs):
+        sql = "SELECT * FROM menu_items"
+        if kwargs:
+            sql += " WHERE "
+            for key, value in kwargs.items():
+                sql += "%s = '%s' AND " % (key, value)
+            sql = sql[:-5]
+        print(sql)
+        return self.__execute_sql(sql)
+
+    def update_menu_item(self, item_name, **kwargs):
+        sql = "UPDATE menu_items SET "
         for key, value in kwargs.items():
-            sql += "%s = '%s' AND " % (key, value)
-        sql = sql[:-5]
-    print(sql)
-    return execute_sql(sql)
+            sql += "%s = '%s', " % (key, value)
+        sql = sql[:-2] + " WHERE name = '%s'" % item_name
+        return self.__execute_sql(sql)
 
-def update_menu_item(item_name, **kwargs):
-    sql = "UPDATE menu_items SET "
-    for key, value in kwargs.items():
-        sql += "%s = '%s', " % (key, value)
-    sql = sql[:-2] + " WHERE name = '%s'" % item_name
-    return execute_sql(sql)
-
-def delete_menu_item(name):
-    sql = """UPDATE menu_items SET active = 0 
-    WHERE menu_items.name = '%s'""" % name
-    return execute_sql(sql)
+    def delete_menu_item(self, name):
+        sql = """UPDATE menu_items SET active = 0 
+        WHERE menu_items.name = '%s'""" % name
+        return self.__execute_sql(sql)
 
 def init_tables():
     conn = sqlite3.connect('orders.db')
@@ -69,8 +71,9 @@ def init_tables():
 
 
 if __name__ == '__main__':
+    menu = Menu()
     try:
-        print(view_orders(1))
+        print(menu.view_orders(1))
     except sqlite3.OperationalError as e:
         print(e)
         conn = sqlite3.connect('orders.db')
@@ -84,4 +87,4 @@ if __name__ == '__main__':
                 conn.close()
             except Exception as e:
                 print(e)
-        print(view_orders(1))
+        print(menu.view_orders(1))
