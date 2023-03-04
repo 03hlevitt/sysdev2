295e618e24728c1cd1b6fdcd308a2cf4b333f801
Harrison Levitt <03hlevitt@gmail.com>
2023-03-04 14:08:29 +0000

    red

--- backend/order.py
+++ backend/order.py
@@ -1,16 +1,29 @@
 """all interactions with orders"""
+import datetime
+
 
 class Order:
-    def __init__(self, name=None, location=None, orders=None) -> None:
+    def __init__(self, customer_id=None, location=None, order_id=None) -> None:
+        self.date = None
+        pass
+    
+    @property
+    def order_id(self):
+        return self.order_id
+
+    def set_order_date(self):
+        self.date = datetime.datetime.utcnow() 
+
+    def add_items(self, name, quantity):
         pass
 
-    def add_item(self):
+    def remove_items(self):
         pass
 
-    def remove_item(self):
+    def view_orders(self):
         pass
 
-    def view_order(self):
+    def view_order_items(self):
         pass
 
     def save(self):
@@ -20,4 +33,19 @@ class Order:
         pass
 
     def get_total(self):
+        pass
+
+class orderItems:
+    def __init__(self, name=None, quanity=None, order_id=None) -> None:
+        self.name = name
+        self.quanity = quanity
+        self.order_id = order_id
+
+    def view_order_items(self):
+        pass
+
+    def save(self):
+        pass
+
+    def delete(self):
         pass
