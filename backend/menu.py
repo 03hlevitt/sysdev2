2e6c524f29510a68c6d985b4fc9b1f1fb632322e
Harrison Levitt <03hlevitt@gmail.com>
2023-03-05 10:26:18 +0000

    menu is now green...

--- backend/menu.py
+++ backend/menu.py
@@ -7,18 +7,10 @@ from backend.common import DBClass
 class Menu(DBClass):
     def __init__(self, name=None, price=None):
         super().__init__()
-        self.name = name
-        self.price = price
 
     def view_menu(self):
         """view the menu"""
         return self.execute_sql("SELECT * FROM menu_items")
-
-    def get_menu_item(self, kwargs):
-        """get a menu item from the db and stoer it in the object"""
-        data = self.execute_sql("SELECT * FROM menu_items WHERE name = '%s'" % kwargs)
-        self.name = data[0][0]
-        self.price = data[0][1]
 
     def save(self):
         """save the menu item stored in the object"""
@@ -27,4 +19,21 @@ class Menu(DBClass):
     def delete_from_db(self):
         """delete the menu item stored in the object"""
         self.execute_sql("DELETE FROM menu_items WHERE name = '%s'" % self.name)
-    
+
+class newMenuItem(Menu):
+    def __init__(self, name, price):
+        super().__init__(name, price)
+        self.name = name
+        self.price = price
+
+class existingMenuItem(Menu):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    @property
+    def price(self):
+        """get a menu item from the db and store it in the object"""
+        data = self.execute_sql("SELECT price FROM menu_items WHERE name = '%s'" % self.name)
+        print(data)
+        return data[0][0]
--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,32 +1,26 @@
 """test the menu class"""
 import pytest
-from backend.menu import Menu
+from backend.menu import newMenuItem, existingMenuItem, Menu
 
-def test_menu():
-    menu = Menu()
-    assert menu.name == None
-    assert menu.price == None
 
 def test_menu_with_args():
-    menu = Menu("test", 1)
+    menu = newMenuItem("test", 1)
     assert menu.name == "test"
     assert menu.price == 1
 
 def test_save_menu():
-    menu = Menu("test", 1)
-    menu.delete_from_db()
-    menu.save()
-    menu.get_menu_item("test")
-    assert menu.name == "test"
-    assert menu.price == 1
+    new_item = newMenuItem("test", 1)
+    new_item.delete_from_db()
+    new_item.save()
+    existing_item = existingMenuItem("test")
+    assert existing_item.name == "test"
+    assert existing_item.price == 1
 
 def test_view_menu():
-    menu = Menu("test", 1)
-    menu.delete_from_db()
-    menu.save()
+    menu = Menu()
     assert menu.view_menu() == [("test", 1)]
 
 def test_delete_from_db_menu():
-    menu = Menu("test", 1)
-    menu.delete_from_db()
-    assert menu.view_menu() == []
+    existing_item = existingMenuItem("test")
+    existing_item.delete_from_db()
+    assert existing_item.view_menu() == []
