d2bfaa20015762959627e39d86cc803a3c88838a
Harrison Levitt <03hlevitt@gmail.com>
2023-03-05 18:31:52 +0000

    separation

---
+++ frontend/order_page.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+
+class HomePage:
+    def __init__(self):
+        self.root_home = Tk()
+        self.root_home.title("home Page")
+        self.root_home.geometry("600x400")
