97656b2edfdc94751e57296f17415b91653cc657
Harrison Levitt <03hlevitt@gmail.com>
2023-03-07 11:50:31 +0000

    nice!

--- backend/menu.py
+++ backend/menu.py
@@ -10,10 +10,6 @@ class Menu(DBClass):
         """view the menu"""
         return self.execute_sql("SELECT * FROM menu_items")
 
-    def save(self):
-        """save the menu item stored in the object"""
-        self.execute_sql("INSERT INTO menu_items (name, price) VALUES ('%s', '%s')" % (self.name, self.price))
-
     def delete_from_db(self):
         """delete the menu item stored in the object"""
         self.execute_sql("DELETE FROM menu_items WHERE name = '%s'" % self.name)
@@ -24,19 +20,26 @@ class newMenuItem(Menu):
         self.name = name
         self.price = price
 
+    def save(self):
+        """save the menu item stored in the object"""
+        self.execute_sql("INSERT INTO menu_items (name, price) VALUES ('%s', '%s')" % (self.name, self.price))
+
 class existingMenuItem(Menu):
     def __init__(self, name):
         super().__init__()
         self.name = name
-
-    @property
-    def price(self):
+        self.price = self._get_price()
+    
+    def _get_price(self):
         """get a menu item from the db and store it in the object"""
         data = self.execute_sql("SELECT price FROM menu_items WHERE name = '%s'" % self.name)
         print(data)
         return data[0][0]
     
     # setter for price
-    @price.setter
     def update_price(self, price):
         self.price = price
+
+    def save(self):
+        """save the menu item stored in the object"""
+        self.execute_sql("UPDATE menu_items SET price = '%s' WHERE name = '%s'" % (self.price, self.name))
